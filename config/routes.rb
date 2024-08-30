a8825cc16b06cf67af2a9adc31d22d2cc5843e64
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-08-30 13:17:39 +0000

    ユーザー詳細ページ雛型・マイページへのリンク設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/unsubscribe'
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-  
+  resources :users, only: [:index,:show, :edit, :unsubscribe]
 end
