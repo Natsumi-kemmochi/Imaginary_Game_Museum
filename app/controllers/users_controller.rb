e3b98ec094a74897c4bc066173b4dcf511647d87
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-08-31 11:33:13 +0000

    ユーザーに画像を設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :introduction, :link)
+    params.require(:user).permit(:name, :introduction, :link, :profile_image)
   end
   
   def is_matching_login_user
