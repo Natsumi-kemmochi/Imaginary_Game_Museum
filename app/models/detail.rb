5a6b151a7be022e2be5aed0d34404c45b22f817a
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-09-14 10:58:29 +0000

    詳細画像エラーの修正

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -4,6 +4,16 @@ class Detail < ApplicationRecord
   
   belongs_to :game
 
+  validates :image, presence: true
   validates :sub_text, length: { maximum: 800 }
+  
+    def get_image(width, height)
+      unless image.attached?
+        #以下の追記を無視し、画像を表示しない
+        #file_path = Rails.root.join('app/assets/images/no_image.jpg')
+        #image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      end
+        image.variant(resize_to_limit: [width, height]).processed
+    end
     
 end
