b8e2f75c225ebde3022bfc36cf6f4eee1385d758
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-09-26 14:18:49 +0000

    テストデータ１完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,18 +28,44 @@ end
   user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.png"), filename:"sample-user3.png")
 end
 
-Game.find_or_create_by!(title: "勇者の冒険") do |game|
+Game.find_or_create_by!(id: "1") do |game|
+  game.title = "勇者の冒険"
   game.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.png"), filename:"sample-post1.png")
   game.caption = "古き良きPRG"
   game.main_text = "設定は遠慮なく生やしてください"
   game.user_id = 1
 end
 
-Game.find_or_create_by!(title: "ゾンビファーム") do |game|
+Game.find_or_create_by!(id: "2") do |game|
+  game.title = "ゾンビファーム"
   game.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.png"), filename:"sample-post2.png")
   game.caption = "ゾンビ育成ゲーム"
   game.main_text = "育てたゾンビと共に未開の地を生きる"
   game.user_id = 2
+end
+
+Game.find_or_create_by!(id: "3") do |game|
+  game.title = "Hakoniwa"
+  game.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.png"), filename:"sample-post3.png")
+  game.caption = "自分だけの素敵な箱庭を作ろう！"
+  game.main_text = ""
+  game.user_id = 3
+end
+
+Game.find_or_create_by!(id: "4") do |game|
+  game.title = "陰に潜むなにか"
+  game.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post4.jpg"), filename:"sample-post4.jpg")
+  game.caption = "純和風ホラー"
+  game.main_text = ""
+  game.user_id = 1
+end
+
+Game.find_or_create_by!(title: "5") do |game|
+  game.title = "妖精の国"
+  game.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post5.jpg"), filename:"sample-post5.jpg")
+  game.caption = "儚く美しい物語"
+  game.main_text = ""
+  game.user_id = 1
 end
 
 Detail.find_or_create_by!(id: "1") do |detail|
@@ -51,6 +77,24 @@ end
 Detail.find_or_create_by!(id: "2") do |detail|
   detail.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1-2.png"), filename:"sample-post1-2.png")
   detail.sub_text = "頼れる仲間たち"
+  detail.game_id = 1
+end
+
+Detail.find_or_create_by!(id: "3") do |detail|
+  detail.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1-3.png"), filename:"sample-post1-3.png")
+  detail.sub_text = "このモンスターを　⇒倒す？／⇒仲間にする"
+  detail.game_id = 1
+end
+
+Detail.find_or_create_by!(id: "4") do |detail|
+  detail.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1-4.png"), filename:"sample-post1-4.png")
+  detail.sub_text = "どうして魔王はこの世界を支配したいんだろう？"
+  detail.game_id = 1
+end
+
+Detail.find_or_create_by!(id: "5") do |detail|
+  detail.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1-5.png"), filename:"sample-post1-5.png")
+  detail.sub_text = "上位存在の目的とは……"
   detail.game_id = 1
 end
 
