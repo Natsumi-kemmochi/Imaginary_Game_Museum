aea39a91ff263893e4ec4eef5943413073498c31
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-10-01 11:54:28 +0000

    [feature/commnent]エラーメッセージの表示

--- app/controllers/game_comments_controller.rb
+++ app/controllers/game_comments_controller.rb
@@ -1,11 +1,15 @@
 class GameCommentsController < ApplicationController
 
   def create
-    game = Game.find(params[:game_id])
-    comment = current_user.game_comments.new(game_comment_params)
-    comment.game_id = game.id
-    comment.save
-    redirect_to game_path(game.id)
+    @game = Game.find(params[:game_id])
+    @game_comment = current_user.game_comments.new(game_comment_params)
+    @game_comment.game_id = @game.id
+    if @game_comment.save
+      redirect_to game_path(@game.id)
+    else
+      @details = @game.details.page(params[:page])
+      render "games/show"
+    end
   end
   
   def destroy
