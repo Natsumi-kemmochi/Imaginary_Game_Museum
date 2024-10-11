9d6468bf3d3226858258f2b7454400373c10bd5d
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-10-11 12:04:33 +0000

    bookmark/indexのルーティング、ページ雛型作成・コントローラ調整中

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -7,6 +7,10 @@ class Public::BookmarksController < ApplicationController
     redirect_back fallback_location: root_path
   end
 
+  def index
+    @games = current_user.games.page(params[:page]).order(created_at: :desc)
+  end
+
   def destroy
     game = Game.find(params[:game_id])
     bookmark = current_user.bookmarks.find_by(game_id: game.id)
