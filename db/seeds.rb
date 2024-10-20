b4c431f293fc91d935a62136bd14b760fca7012b
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-10-20 09:24:55 +0000

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,6 +55,7 @@ Game.find_or_create_by!(id: "1") do |game|
   game.caption = "友情と運命が交錯する冒険。若き勇者アレンの結末は"
   game.main_text = "若き勇者アレンが故郷の村を守るため、未知の世界へ旅立つ。村を襲った魔物の影響で村人たちは恐れおののき、アレンは祖父から受け継いだ剣を手に、伝説の魔法使いを探す決意をする。 旅の途中、アレンは弓使いリナ、戦士カイル、謎めいた魔法使いセリーナと出会う。彼らはそれぞれの目的を抱えつつ、共に旅をする。仲間たちは試練を乗り越え、友情を深めながら過去を明かしていく。 しかし、アレンの前には常に謎めいた影が立ちはだかり、真の敵は誰なのか分からない。暗黒の魔王の存在が囁かれ、彼の目的には大きな秘密が隠されている。アレンは村人たちを守るための強い思いを抱き、運命の城へ近づいていく。 果たして、彼らは真の敵に立ち向かうことができるのか。アレンの選択が村の未来をどう変えるのか、旅の行く先には未知の運命が待っている。仲間との絆を信じ、最後の試練に挑む。"
   game.user_id = 1
+  game.tag = "RPG 剣　魔法"
 end
 
 Game.find_or_create_by!(id: "2") do |game|
@@ -63,6 +64,7 @@ Game.find_or_create_by!(id: "2") do |game|
   game.caption = "育てたゾンビと共に未開の地を生きる"
   game.main_text = "「ゾンビファーム」は、育てたゾンビたちと共に未開の地でのんびり生活するシミュレーションゲーム。プレイヤーは、個性豊かなゾンビを育成し、農業や資源収集を行いながら、独自のファームを築く。舞台は自然豊かな不思議な土地。ゾンビたちに食べ物を与えたり、特別なアイテムで成長を促したりして、彼らを強化していく。各ゾンビは異なるスキルや特性を持ち、農業や冒険に役立てられる。可愛らしい見た目のゾンビたちは、ユニークなアニメーションでプレイヤーを楽しませる。時には予想外の行動を取って、思わず笑ってしまう瞬間も。"
   game.user_id = 2
+  game.tag = "ゾンビ　育成　ほのぼの"
 end
 
 Game.find_or_create_by!(id: "3") do |game|
@@ -71,17 +73,27 @@ Game.find_or_create_by!(id: "3") do |game|
   game.caption = "自分だけの素敵な箱庭を作ろう！"
   game.main_text = "「Hakoniwa」は、自分だけの素敵な箱庭を自由に作り上げるシミュレーションゲーム。美しい自然や可愛らしいキャラクターたちと共に、平和な世界を築くことも、プレイスタイル次第で世紀末的な荒廃した風景を創り出すことも可能。土地をカスタマイズし、建物や植物、キャラクターを配置して、思い描く世界を実現しよう。他のプレイヤーとの交流や、共同でイベントを楽しむ要素も豊富。個性的なデザインやアイディアを活かして、自分だけの箱庭を作り上げる楽しさが詰まった作品だ。どんな箱庭が生まれるのか、無限の可能性が待っている。"
   game.user_id = 3
+  game.tag = "サンドボックス"
 end
 
 Game.find_or_create_by!(id: "4") do |game|
+  game.title = "魔王の冒険"
+  game.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1-4.png"), filename:"sample-post1-4.png")
+  game.caption = "今度はワシが主役！"
+  game.user_id = 1
+  game.tag = "RPG 魔王"
+end
+
+Game.find_or_create_by!(id: "5") do |game|
   game.title = "陰に潜むなにか"
   game.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post4.jpg"), filename:"sample-post4.jpg")
   game.caption = "真実を求め、闇の中へ"
   game.main_text = "古い神社で起こる奇怪な現象に巻き込まれる主人公。村の伝説や噂話に耳を傾けながら、夜ごとに現れる影や不気味な声が、村人たちの心に恐怖を植え付ける。友人や家族の失踪。村の過去に隠された秘密。果たして、その「なにか」の正体を突き止め、村を救うことができるのか。影が忍び寄る中、謎は深まるばかり。"
-  game.user_id = 1
+  game.user_id = 4
+  game.tag = "ホラー　ノベル"
 end
 
-Game.find_or_create_by!(id: "5") do |game|
+Game.find_or_create_by!(id: "6") do |game|
   game.title = "妖精の国"
   game.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post5.jpg"), filename:"sample-post5.jpg")
   game.caption = "失われた魔法を取り戻し、妖精たちの国に再び光を"
@@ -89,7 +101,13 @@ Game.find_or_create_by!(id: "5") do |game|
   game.user_id = 1
 end
 
-
+Game.find_or_create_by!(id: "7") do |game|
+  game.title = "スライムの冒険"
+  game.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1-3.png"), filename:"sample-post1-3.png")
+  game.caption = "魔王軍3度目の正直！　……そろそろ無理がある？"
+  game.user_id = 1
+  game.tag = "RPG スライム　かわいい　癒し"
+end
 
 # ---------詳細記事-------------
 
