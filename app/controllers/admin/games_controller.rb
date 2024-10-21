d2e4f21882c4e74f47ecd78a900f5fa9a9fc2466
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-10-21 10:48:24 +0000

    管理者機能追加(ゲーム情報)

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -1,7 +1,33 @@
 class Admin::GamesController < ApplicationController
-    def destroy
-        @game = Game.find(params[:id])
-        @game.destroy
-        redirect_to admin_game_dashboards_path, notice: 'ゲームを削除しました。'
+  layout 'admin'
+  
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    if  @game.update(game_params)
+        flash[:notice] = "You have updated game successfully."
+        redirect_to  edit_admin_game_path(@game.id)
+    else
+      render :edit
     end
+  end
+
+  
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to admin_game_dashboards_path, notice: 'ゲームを削除しました。'
+  end
+
+
+
+  private
+   
+  def game_params
+    params.require(:game).permit(:title, :caption, :main_text, :image, :tag)
+  end
+
 end
