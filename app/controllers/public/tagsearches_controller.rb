934c743d5cec118127ea1e078df3b412d8bba8ca
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-10-29 11:50:19 +0000

    tagエラーメッセージ修正

--- app/controllers/public/tagsearches_controller.rb
+++
@@ -1,9 +0,0 @@
-class  Public::TagsearchesController < ApplicationController
-  
-  def search
-    @model = Game
-    @word = params[:content]
-    @games = Game.where("tag LIKE?","%#{@word}%").page(params[:page]).order(created_at: :desc)
-    render "public/tagsearches/search"
-  end
-end
--- app/helpers/tagsearches_helper.rb
+++
@@ -1,2 +0,0 @@
-module TagsearchesHelper
-end
