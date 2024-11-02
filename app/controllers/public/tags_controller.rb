dd75b98b7f05338d5b081cb74c4f9ced7d8cc346
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-11-02 07:20:45 +0000

    タグ一覧を人気順に変更

--- app/controllers/public/tags_controller.rb
+++ app/controllers/public/tags_controller.rb
@@ -2,7 +2,7 @@ class Public::TagsController < ApplicationController
 
   def index
       @tags = Tag.game_count.limit(10)
-      @all_tags = Tag.all
+      @all_tags = Tag.all.game_count
   end
 
 end
